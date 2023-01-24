5852c6e1077fb2490024fc1dfb14949a3ca8a3be
Kamil Gapiński <kagie11@yahoo.com>
2023-01-24 19:10:47 +0100

    Day 1 Add HTML Models

--- Blog/blogwebsite/admin.py
+++ Blog/blogwebsite/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.site.register(Post)
+
+# @admin.register(Post)
+# class PostListAdmin(admin.ModelAdmin):
+#     display_list = ['title', 'hashtags', 'author', 'publish', 'status']
+#     filter_list = ['status', 'created', 'publish', 'author']
+    
--- Blog/blogwebsite/models.py
+++ Blog/blogwebsite/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    class PostStatus(models.TextChoices):
+        PROJECT = 'PR', 'Project'
+        PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=256)
+    hashtags = models.CharField(max_length=256, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices= PostStatus.choices,
+                              default = PostStatus.PROJECT)
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish'])]
+
+    def __str__(self):
+        return self.title
