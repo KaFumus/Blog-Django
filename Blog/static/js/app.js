cb02f76616ec17c6687d2f4f6b1537980daf958a
Kamil Gapiński <kagie11@yahoo.com>
2023-01-25 15:43:53 +0100

    Day 2 Correct HTML

---
+++ Blog/static/js/app.js
@@ -0,0 +1,25 @@
+// // Get the signup link
+// let signupLink = document.getElementById("signup-link");
+
+// // Get the login link
+// let loginLink = document.getElementById("login-link");
+
+// // Get the login form
+// let loginForm = document.querySelector(".form-container");
+
+// // Get the signup form
+// let signupForm = document.getElementById("signup-form");
+
+// // Add click event to signup link
+// signupLink.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loginForm.style.display = "none";
+//   signupForm.style.display = "block";
+// });
+
+// // Add click event to login link
+// loginLink.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   loginForm.style.display = "block";
+//   signupForm.style.display = "none";
+// });
